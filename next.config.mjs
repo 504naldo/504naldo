2c358283a075d5903e698af8eabe4de89d9f6499
504naldo <128623633+504naldo@users.noreply.github.com>
2025-09-28 00:37:19 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
 import withPWA from 'next-pwa';
 
-const isDev = process.env.NODE_ENV !== 'production';
-
 const config = {
   reactStrictMode: true,
-  experimental: { appDir: true },
 };
 
 export default withPWA({
   dest: 'public',
-  disable: isDev, // enable Service Worker only in production
+  disable: process.env.NODE_ENV !== 'production',
   register: true,
   skipWaiting: true,
 })(config);
